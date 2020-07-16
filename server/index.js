ace02af42763a6b35db04620423073df530bf26c
Jayalakshmi <jayalakshmi.j@codingmart.com>
2020-07-16 19:59:27 +0530

    db added

--- server/index.js
+++ server/index.js
@@ -4,6 +4,18 @@ const generatePassword = require("password-generator");
 
 const app = express();
 
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "lpxotafiwicylh",
+  host: "ec2-52-204-232-46.compute-1.amazonaws.com",
+  database: "d14qhcrq07gtub",
+  password: "cef7f1533511e57830bc7888ce400fd57de374904208fafdf6b1e98c3e85d776",
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
 
@@ -15,6 +27,19 @@ app.get("/api/passwords", (req, res) => {
   const passwords = Array.from(Array(count).keys()).map((i) =>
     generatePassword(12, false)
   );
+
+  app.get("/db", async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM sample");
+      const results = { results: result ? result.rows : null };
+      res.render("pages/db", results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  });
 
   // Return them as json
   res.json(passwords);
